bedbec46916a9b592b618ac567ba0777f969f8cf
NoahIT <rascius.nojus@gmail.com>
2022-03-22 19:56:09 +0200

    15-Pask done

---
+++ 15-Pask (Cross the road game)/main.py
@@ -0,0 +1,34 @@
+from turtle import Screen
+from player import Player
+import time
+from scoreboard impoert Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+scoreboard =
+
+player = Player()
+
+screen.listen()
+screen.onkey(player.go_up, "Up")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_in_on = False
+            scoreboard.game_over()
+
+    if player.is_at_finish():
+        player.go_to_start()
+        car_manager.level_up()
+        scoreboard.increase_level()
+
+screen.exitonclick()
